33a06ccb28085355d4cfed14916ed942ddec28b0
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-22 13:32:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split()
   new_array = [ ]
-  array.each { |word| new_array << word.reverse }
+  array.collect { |word| new_array << word.reverse }
   new_array
   new_array.join(" ")
 end
